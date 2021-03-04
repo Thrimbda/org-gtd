df3be4599d273989a833a376fbae7fd760b4fe83
Siyuan Wang <c.one@thrimbda.com>
2021-03-04 15:01:41 +0800

    made my own version

--- packages.el
+++ packages.el
@@ -80,7 +80,7 @@
 
   (if (boundp 'org-user-agenda-files)
       (setq org-agenda-files org-user-agenda-files)
-    (setq org-agenda-files (quote ("~/git/org"))))
+    (setq org-agenda-files (quote ("~/OneDrive/cone"))))
 
   ;; Do not dim blocked tasks
   (setq org-agenda-dim-blocked-tasks nil)
@@ -423,7 +423,7 @@ so change the default 'F' binding in the agenda to allow both"
   (setq org-agenda-skip-scheduled-if-deadline-is-shown  (quote repeated-after-deadline))
 
   (setq org-agenda-include-diary nil)
-  (setq org-agenda-diary-file "~/git/org/diary.org")
+  (setq org-agenda-diary-file "~/OneDrive/cone/diary.org")
   (setq org-agenda-insert-diary-extract-time t)
 
   ;; Include agenda archive files when searching for things
@@ -434,7 +434,7 @@ so change the default 'F' binding in the agenda to allow both"
   (spacemacs|use-package-add-hook org
     :post-config
     (progn
-      (setq org-default-notes-file "~/git/org/refile.org")
+      (setq org-default-notes-file "~/OneDrive/cone/refile.org")
 
       (require 'org-id)
       (defun bh/clock-in-task-by-id (id)
@@ -545,26 +545,26 @@ so change the default 'F' binding in the agenda to allow both"
                 ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
                 ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
 
-  (setq org-directory "~/git/org")
+  (setq org-directory "~/OneDrive/cone")
 
   ;; Capture templates for: TODO tasks, Notes, appointments, phone calls,
   ;; meetings, and org-protocol
   (setq org-capture-templates
-        (quote (("t" "todo" entry (file "~/git/org/refile.org")
+        (quote (("t" "todo" entry (file "~/OneDrive/cone/refile.org")
                  "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-                ("r" "respond" entry (file "~/git/org/refile.org")
+                ("r" "respond" entry (file "~/OneDrive/cone/refile.org")
                  "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
-                ("n" "note" entry (file "~/git/org/refile.org")
+                ("n" "note" entry (file "~/OneDrive/cone/refile.org")
                  "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-                ("j" "Journal" entry (file+datetree "~/git/org/diary.org")
+                ("j" "Journal" entry (file+datetree "~/OneDrive/cone/diary.org")
                  "* %?\n%U\n" :clock-in t :clock-resume t)
-                ("w" "org-protocol" entry (file "~/git/org/refile.org")
+                ("w" "org-protocol" entry (file "~/OneDrive/cone/refile.org")
                  "* TODO Review %c\n%U\n" :immediate-finish t)
-                ("m" "Meeting" entry (file "~/git/org/refile.org")
+                ("m" "Meeting" entry (file "~/OneDrive/cone/refile.org")
                  "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-                ("p" "Phone call" entry (file "~/git/org/refile.org")
+                ("p" "Phone call" entry (file "~/OneDrive/cone/refile.org")
                  "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-                ("h" "Habit" entry (file "~/git/org/refile.org")
+                ("h" "Habit" entry (file "~/OneDrive/cone/refile.org")
                  "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
 
   ;; Remove empty LOGBOOK drawers on clock out
@@ -771,7 +771,7 @@ A prefix arg forces clock in of the default task."
                               ("WORK" . ?W)
                               ("FARM" . ?F)
                               ("ORG" . ?O)
-                              ("NORANG" . ?N)
+                              ("BYTEDANCE" . ?B)
                               ("crypt" . ?E)
                               ("NOTE" . ?n)
                               ("CANCELLED" . ?c)
@@ -1087,15 +1087,11 @@ Skip project and sub-project tasks, habits, and loose non-project tasks."
    (quote ((emacs-lisp . t)
            (dot . t)
            (ditaa . t)
-           (R . t)
            (python . t)
-           (ruby . t)
-           (gnuplot . t)
-           (clojure . t)
            (shell . t)
            (ledger . t)
            (org . t)
-           (plantuml . t)
+           (ammonite . t)
            (latex . t))))
 
   ;; Do not prompt to confirm evaluation
