9120b438fa560a6d8311a7942e3be9dcf9427967
Siyuan Wang <c.one@thrimbda.com>
2021-03-08 20:10:54 +0800

    sync my config

--- packages.el
+++ packages.el
@@ -587,13 +587,13 @@ so change the default 'F' binding in the agenda to allow both"
   (setq org-refile-use-outline-path t)
 
   ;; ;; Targets complete directly with IDO
-  ;; (setq org-outline-path-complete-in-steps nil)
+  (setq org-outline-path-complete-in-steps nil)
 
   ;; Allow refile to create parent tasks with confirmation
   (setq org-refile-allow-creating-parent-nodes (quote confirm))
 
   ;;   ;; ;; Use IDO for both buffer and file completion and ido-everywhere to t
-  ;;   ;; (setq org-completion-use-ido t)
+  (setq org-completion-use-ido nil)
   ;;   ;; (setq ido-everywhere t)
   ;;   ;; (setq ido-max-directory-size 100000)
   ;;   ;; (ido-mode (quote both))
