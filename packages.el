71479e73bd9e5e2075228ce948138e1a7c32db56
Siyuan Wang <c.one@thrimbda.com>
2021-03-15 11:13:06 +0800

    feat: update effort estimate template

--- packages.el
+++ packages.el
@@ -756,7 +756,7 @@ A prefix arg forces clock in of the default task."
         "%50ITEM(Task) %10TODO %3PRIORITY %TAGS %10Effort(Effort){:} %10CLOCKSUM")
   ;; global Effort estimate values
   ;; global STYLE property values for completion
-  (setq org-global-properties (quote (("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
+  (setq org-global-properties (quote (("Effort_ALL" . "0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00")
                                       ("STYLE_ALL" . "habit"))))
   ;; Tags with fast selection keys
   (setq org-tag-alist (quote ((:startgroup)
